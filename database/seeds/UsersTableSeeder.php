5c342b30e2793b705d2386b6417033723837e511
Hreinosa <hreinosa@hotmail.com>
2019-02-05 17:47:00 -0600

    seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,9 +16,17 @@ class UsersTableSeeder extends Seeder
         $user = User::create([
             'name'=>'Administrador',
             'email'=>'administrador@proyectojuntos.com',
-            'company'=>'Empresa A',
+            'company'=>'GRUPO BORJA',
             'password'=>Hash::make('123456'),
             'role'=>0 //Administrador
+        ]);
+
+        $user = User::create([
+            'name'=>'Coach',
+            'email'=>'coach@proyectojuntos.com',
+            'company'=>'GRUPO BORJA',
+            'password'=>Hash::make('123456'),
+            'role'=>1 //Coach
         ]);
 
        
